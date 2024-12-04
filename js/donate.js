7a7ab8202b2b08600ee6b8199cef14b8faa5c421
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-12-04 20:01:04 +0600

    total donation add

--- js/donate.js
+++ js/donate.js
@@ -4,26 +4,30 @@ document.getElementById('donateByNoakhali').addEventListener('click', function(e
    e.preventDefault();
    const FloodDonate = InPutFieldId('FloodDonate-filed');
    const currentDonate  = textTypeId('Flood-donate');
+   const totalDonation = textTypeId('total-donation');
    const donate = currentDonate + FloodDonate;
    document.getElementById('Flood-donate').innerText = donate;
    document.getElementById('FloodDonate-filed').value = '';
+   document.getElementById('total-donation').innerText = totalDonation + donate;
 })
 document.getElementById('Flood-Relief-btn').addEventListener('click', function(e){
    e.preventDefault();
    const FloodReliefDonate = InPutFieldId('Flood-Relief-filed');
    const currentDonate2  = textTypeId('Flood-Relief');
+   const totalDonation = textTypeId('total-donation');
    const donate2 = currentDonate2 + FloodReliefDonate;
    document.getElementById('Flood-Relief').innerText = donate2;
    document.getElementById('Flood-Relief-filed').value = '';
+   document.getElementById('total-donation').innerText = totalDonation + donate2;
 })
 document.getElementById('Quota-Movement-btn').addEventListener('click', function(e) {
     e.preventDefault(); 
     const quotaMovementDonate = InPutFieldId('Quota-Movement-filed');
     const currentDonate = textTypeId('donate-Quota');
+    const totalDonation = textTypeId('total-donation');
     console.log(quotaMovementDonate,currentDonate);
-    const totalDonate = currentDonate + quotaMovementDonate;
-    console.log(totalDonate);
-    document.getElementById('donate-Quota').innerText = totalDonate;
+    const totalDonate1 = currentDonate + quotaMovementDonate;
+    document.getElementById('donate-Quota').innerText = totalDonate1;
     document.getElementById('Quota-Movement-filed').value = '';
+    document.getElementById('total-donation').innerText = totalDonation + totalDonate1 ;
 });
-
