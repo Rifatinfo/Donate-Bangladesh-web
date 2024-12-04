bc100a229e87febbfa65599a2b4644dc4d4a10ec
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-12-04 19:20:08 +0600

    add all donation

--- js/donate.js
+++ js/donate.js
@@ -3,22 +3,27 @@
 document.getElementById('donateByNoakhali').addEventListener('click', function(e){
    e.preventDefault();
    const FloodDonate = InPutFieldId('FloodDonate-filed');
-   const currentDonate  = textTypeId('donate-Quota');
+   const currentDonate  = textTypeId('Flood-donate');
    const donate = currentDonate + FloodDonate;
-//    document.getElementById('donate-Quota').innerText = donate;
+   document.getElementById('Flood-donate').innerText = donate;
+   document.getElementById('FloodDonate-filed').value = '';
 })
 document.getElementById('Flood-Relief-btn').addEventListener('click', function(e){
    e.preventDefault();
    const FloodReliefDonate = InPutFieldId('Flood-Relief-filed');
    const currentDonate2  = textTypeId('Flood-Relief');
    const donate2 = currentDonate2 + FloodReliefDonate;
-//    document.getElementById('Flood-Relief').innerText = donate2;
+   document.getElementById('Flood-Relief').innerText = donate2;
+   document.getElementById('Flood-Relief-filed').value = '';
 })
-document.getElementById('Quota-Movement-btn').addEventListener('click', function(e){
-   e.preventDefault();
-   const QuotaMovementDonate = InPutFieldId('Quota-Movement-filed');
-   const currentDonate3  = textTypeId('Flood-donate');
-   const donate3 = currentDonate3 + QuotaMovementDonate;
-//    document.getElementById('Flood-donate').innerText = donate3;
-})
+document.getElementById('Quota-Movement-btn').addEventListener('click', function(e) {
+    e.preventDefault(); 
+    const quotaMovementDonate = InPutFieldId('Quota-Movement-filed');
+    const currentDonate = textTypeId('donate-Quota');
+    console.log(quotaMovementDonate,currentDonate);
+    const totalDonate = currentDonate + quotaMovementDonate;
+    console.log(totalDonate);
+    document.getElementById('donate-Quota').innerText = totalDonate;
+    document.getElementById('Quota-Movement-filed').value = '';
+});
 
